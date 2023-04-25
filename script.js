73e17578656c948de9136f82c951339414b85ccc
cdeorta <131566774+cdeorta@users.noreply.github.com>
2023-04-24 20:28:53 -0700

    Display open or closed feature

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("JavaScript file linked");
+
+const openingHour = 9;
+const closingHour = 17;
+
+  // Get the current hour in 24-hour format
+const now = new Date();
+const currentHour = now.getHours();
+
+  // Check if the business is open or closed
+if (currentHour >= openingHour && currentHour < closingHour) {
+    document.getElementById("status").innerHTML = "We're open!";
+ } else {
+    document.getElementById("status").innerHTML = "Sorry, we're closed.";
+  }
