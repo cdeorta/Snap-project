f05841304abec1a2f044d464ad4badc2563cb431
cdeorta <131566774+cdeorta@users.noreply.github.com>
2023-04-24 20:44:28 -0700

    adjust for sat/sun hours

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 console.log("JavaScript file linked");
 
-const openingHour = 9;
-const closingHour = 17;
+// Set the opening and closing hours in 24-hour format
+const weekdaysOpeningHour = 9;
+const weekdaysClosingHour = 17;
+const saturdayOpeningHour = 10;
+const saturdayClosingHour = 16;
 
-  // Get the current hour in 24-hour format
+  // Get the current day and hour in 24-hour format
 const now = new Date();
+const currentDay = now.getDay();
 const currentHour = now.getHours();
 
-  // Check if the business is open or closed
-if (currentHour >= openingHour && currentHour < closingHour) {
-    document.getElementById("status").innerHTML = "We're open!";
- } else {
+ // Check if the business is open or closed
+if (currentDay === 0 || currentHour < weekdaysOpeningHour || currentHour >= weekdaysClosingHour) {
+    // Closed on Sundays or outside weekdays hours
     document.getElementById("status").innerHTML = "Sorry, we're closed.";
+  } else if (currentDay === 6 && (currentHour < saturdayOpeningHour || currentHour >= saturdayClosingHour)) {
+    // Closed on Saturdays outside Saturday hours
+    document.getElementById("status").innerHTML = "Sorry, we're closed.";
+  } else {
+    // Open
+    document.getElementById("status").innerHTML = "We're open!";
   }
